25437e69a89e2bb65c722f61c49d60d5028d4ee4
keith musau <keithmusau@gmail.com>
2023-07-05 16:50:40 +0300

    yes

--- website/views.py
+++ website/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html', {})
- 
+
+
+
+def logout_user(request):
+    pass
+
+  

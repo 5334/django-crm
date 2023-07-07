ad55871d30f2fd1fb8a8142a8178063d2d35d7d0
keith musau <keithmusau@gmail.com>
2023-07-07 15:28:09 +0300

    yes

--- website/views.py
+++ website/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    #logic for checking if a user is logged in
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        #Authenticate 
+        user = authenticate(request username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are now logged in!") 
+            return redirect('home')
+        else:
+             messages.success(request, "There was a problem logging you in. Please try again!")
+             return redirect('home')
+    else:    
+        return render(request, 'home.html', {})
 
-#some code was deleted in this line
 
 def logout_user(request):
     pass
